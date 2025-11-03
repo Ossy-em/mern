d5f88d0b042b5b6c312e0ac2b997ded55cdebe92
Ossy-em <emosinachi@gmail.com>
2025-11-03 18:24:10 +0100

    initial commit - BE

---
+++ backend/server.js
@@ -0,0 +1,76 @@
+import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "../config/db.js";
+import Product from "./models/product.model.js";
+import mongoose from "mongoose";
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); //allows us to accept JSON data in the req.body
+
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json({ success: true, data: products });
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+});
+app.post("/api/products", async (req, res) => {
+  const product = req.body; //user sends this data
+  if (!product.name || !product.price || !product.image) {
+    return res
+      .status(400)
+      .json({ success: false, message: "Please provide all fields." });
+  }
+
+  const newProduct = new Product(product);
+
+  try {
+    await newProduct.save();
+    res.status(201).json({ success: true, data: newProduct });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Service error" });
+  }
+});
+
+app.put("/api/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = req.body; //gets us the field
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Invalid Product ID" });
+  }
+
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {
+      new: true,
+    });
+    res.status(200).json({ success: true, data: updatedProduct });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Service error" });
+  }
+});
+app.delete("/api/products/:id", async (req, res) => {
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res
+      .status(404)
+      .json({ success: false, message: "Invalid Product ID" });
+  }
+  const { id } = req.params;
+  try {
+    await Product.findByIdAndDelete(id);
+    res.status(200).json({ success: true, message: "Product deleted" });
+  } catch (error) {
+    res.status(500).json({ success: false, message: "Service error" });
+  }
+});
+app.listen(PORT, () => {
+  connectDB(); //explain call the DB here
+  console.log("server started at http://localhost:" + PORT);
+});
+//Zi7HGsV7BmttXbyA
